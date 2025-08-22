5ca31212c273a47a0fa615dd4f9d0a63a53aaa04
Jan Bláha <blaha.j502@gmail.com>
2025-08-22 19:03:51 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import flet as ft
 from pathlib import Path
+from chessboard import Chessboard
 from ui import MagChessUI
 
 debug = False
-debug = True
+# debug = True
 
 def main(page: ft.Page):
     page.title = "MagChess"
@@ -20,13 +21,18 @@ def main(page: ft.Page):
         "Noto Sans": str(Path(__file__).parent / "assets/fonts/NotoSans-Bold.ttf"),
     }
 
+    # Exit event
     def on_key(e: ft.KeyboardEvent):
         if e.key == "Escape":
             page.window.close()
     page.on_keyboard_event = on_key
 
+    # Build UI
     ui = MagChessUI(page, debug)
     page.add(ui.root)
+
+    # Start chessboard
+    Chessboard(page)
 
 if __name__ == "__main__":
     ft.app(target=main)
--- app/chessboard.py
+++ app/chessboard.py
@@ -3,5 +3,16 @@
 # It also communicates with stockfish and hands analysis information to ui
 # Basically it's a three way bridge between sensors, stockfish and the ui
 
+import asyncio
+import flet as ft
+
 class Chessboard:
-    pass
+    def __init__(self, page: ft.Page):
+        page.run_task(self.update)
+
+    async def update(self):
+        while True:
+
+            print("Update")
+
+            await asyncio.sleep(1/60)
