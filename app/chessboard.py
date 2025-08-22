60be87a1c39ae576ed17c3f8fa1b778d7b8a9a76
Jan Bláha <blaha.j502@gmail.com>
2025-08-22 18:16:53 +0200

    update

---
+++ app/chessboard.py
@@ -0,0 +1,7 @@
+# Chessboard is the game state manager
+# Based on sensor values, this class has to infer where all the pieces are, and call ui updates accordingly
+# It also communicates with stockfish and hands analysis information to ui
+# Basically it's a three way bridge between sensors, stockfish and the ui
+
+class Chessboard:
+    pass
--- app/sensors.py
+++ app/sensors.py
@@ -12,13 +12,15 @@ class SensorSS49E(Sensor):
         pass
 
 class SensorSW(Sensor):
-
     state: int
 
     low_border = 32500
     high_border = 33000
     signal_strength = 500
     noise = 200
+
+    def __init__(self, state: int):
+        self.state = state
 
     def get_value(self):
         if self.state == -1:
@@ -32,14 +34,18 @@ class SensorSW(Sensor):
         self.state = state
 
 class Sensors:
-
     sensors: dict[tuple[int, int], Sensor]
 
     def __init__(self, emulator: bool):
         if emulator:
-            for x in range(8):
-                for y in range(8):
-                    self.sensors[(x, y)] = SensorSW()
+            for letter in range(8):
+                for number in range(8):
+                    state = 0
+                    if letter == 0 or letter == 1:
+                        state = 1
+                    if letter == 6 or letter == 7:
+                        state = -1
+                    self.sensors[(letter, number)] = SensorSW(state)
         else:
             # TODO
             pass
