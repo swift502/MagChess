c2c796213561c2a8275beb5b8558b579c36b327e
Jan Bláha <blaha.j502@gmail.com>
2025-08-24 22:42:50 +0200

    committing v1

--- app/chessboard.py
+++ app/chessboard.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 import asyncio
+import typing
 import flet as ft
 
 from cell import Cell
@@ -7,22 +9,31 @@ from data import ColorSwap, DataLib, MissingPiece, NewPiece, SensorProvider
 from piece import Piece
 from ui_instance import MagChessUI
 
+if typing.TYPE_CHECKING:
+    from typing_extensions import TypeAlias
+
+BoardState: TypeAlias = dict[tuple[int, int], Piece]
+
 class Chessboard:
     board: chess.Board
-    state_stack: list[dict[tuple[int, int], Piece]]
-    staging_state: dict[tuple[int, int], Piece]
+    state_stack: list[BoardState]
+    staging_state: BoardState
 
     cells: dict[tuple[int, int], Cell]
     spawned_pieces: list[Piece]
-    current_player_color: chess.Color
+    current_player: chess.Color
 
-    last_analyzed_sensor_state: str
+    last_analysed_sensor_state: str | None
+    last_legal_move: chess.Move | None
+
+    @property
+    def next_player(self):
+        return chess.WHITE if self.current_player == chess.BLACK else chess.BLACK
 
     def __init__(self, page: ft.Page, ui: MagChessUI, sensors: SensorProvider):
         self.page = page
         self.ui = ui
         self.sensors = sensors
-        self.last_analyzed_sensor_state = ""
         
         self.state_stack = []
         self.staging_state = {}
@@ -76,7 +87,9 @@ class Chessboard:
 
         self.board = chess.Board()
 
-        init_state: dict[tuple[int, int], Piece] = {}
+        init_state: BoardState = {}
+        self.last_analysed_sensor_state = None
+        self.last_legal_move = None
 
         for locator, pieceData in DataLib.start_configuration().items():
             coords = self.locator_to_coords(locator)
@@ -95,7 +108,7 @@ class Chessboard:
             piece.destroy()
         self.pieces = []
 
-        self.current_player_color = chess.WHITE
+        self.current_player = chess.WHITE
 
         # State
         self.state_stack = []
@@ -110,10 +123,13 @@ class Chessboard:
     def coords_to_locator(self, coords: tuple[int, int]):
         return chr(coords[0] + ord("a")) + str(coords[1] + 1)
     
-    def commit_state(self):
+    def commit_staging_state(self, move: chess.Move):
         self.state_stack.append(self.staging_state.copy())
+        self.board.push(move)
+        self.current_player = self.next_player
+        print(f"Committed {move.uci()}")
 
-    def show_state(self, state: dict[tuple[int, int], Piece]):
+    def show_state(self, state: BoardState):
         # Iterate state
         for coords, piece in state.items():
             if piece in self.spawned_pieces:
@@ -134,15 +150,20 @@ class Chessboard:
 
         # Check for changes
         sensor_state = self.get_sensor_state_format()
-        if self.last_analyzed_sensor_state == sensor_state:
+        if self.last_analysed_sensor_state == sensor_state:
             return
 
         # Changes found, start new analysis
-        self.last_analyzed_sensor_state = sensor_state
+        self.last_analysed_sensor_state = sensor_state
+
+        # See if conditions are met to commit the last staging state
+        self.state_commit_processing()
+
+        # Staging state may have been committed now, we can safely discard it
         self.staging_state = self.state_stack[-1].copy()
 
-        # Analyze changes
-        missing, new, swaps = self.analyze_changes()
+        # Analyse changes against latest committed state
+        missing, new, swaps = self.analyse_sensor_changes(against=self.state_stack[-1])
         print(f"Missing {len(missing)}, New {len(new)}, Swaps {len(swaps)}")
 
         # Update staging state
@@ -152,49 +173,76 @@ class Chessboard:
         if uci is not None:
             move = chess.Move.from_uci(uci)
             if move in self.board.legal_moves:
+                self.last_legal_move = move
                 self.ui.update_move_screen(DataLib.icons.good, f"Legal move")
             else:
                 self.ui.update_move_screen(DataLib.icons.invalid, f"Illegal move")
 
-    def analyze_changes(self):
+    def state_commit_processing(self):
+        if self.last_legal_move is None:
+            return
+
+        missing, _, _ = self.analyse_sensor_changes(against=self.staging_state)
+
+        if len(missing) == 1:
+            if missing[0].piece.color == self.next_player:
+                # Other player raised a piece, we have to commit
+                self.commit_staging_state(self.last_legal_move)
+
+    def analyse_sensor_changes(self, against: BoardState):
         missing: list[MissingPiece] = []
         new: list[NewPiece] = []
         swaps: list[ColorSwap] = []
 
         # Find changes
-        for (co_letter, co_number), cell in self.cells.items():
+        for coords, cell in self.cells.items():
             # Get new color from sensor data
             new_color = cell.color
 
             # Get old color from last committed state
-            piece = self.state_stack[-1].get((co_letter, co_number))
+            piece = against.get(coords)
 
             if piece is None:
                 old_color = None
 
                 # Found new
                 if new_color != None:
-                    new.append(NewPiece(new_color, (co_letter, co_number)))
+                    new.append(NewPiece(new_color, coords))
 
             else:
                 old_color = piece.color
 
                 # Found missing
                 if new_color == None:
-                    missing.append(MissingPiece(piece, (co_letter, co_number)))
+                    missing.append(MissingPiece(piece, coords))
 
                 # Found swap
                 elif old_color != new_color:
-                    swaps.append(ColorSwap(piece, new_color, (co_letter, co_number)))
-        
+                    swaps.append(ColorSwap(piece, new_color, coords))
+
         return missing, new, swaps
+
+    # def compare_states(self, a: BoardState, b: BoardState):
+    #     missing: list[MissingPiece] = []
+    #     new: list[NewPiece] = []
+    
+    #     for coords, a_piece in a.items():
+    #         b_piece = b.get(coords)
+    #         if a_piece is b_piece:
+    #             continue
+    #         elif a_piece is not None and b_piece is None:
+    #             missing.append(MissingPiece(a_piece, coords))
+    #         elif a_piece is None and b_piece is not None:
+    #             new.append(NewPiece(b_piece.color, coords))
+
+    #     return missing, new
 
     def update_staging_state(self, missing, new, swaps) -> str | None:
         missing_new_swaps = (len(missing), len(new), len(swaps))
 
         if missing_new_swaps == (1, 1, 0):
-            # Move
             if missing[0].piece.color == new[0].color:
+                # Move
                 return self.staging_move_piece(missing[0], new[0].coords)
             else:
                 # A piece changed color while moving, doesn't make sense
@@ -215,7 +263,7 @@ class Chessboard:
 
         else:
             # No changes
-            if self.current_player_color == chess.WHITE:
+            if self.current_player == chess.WHITE:
                 self.ui.update_move_screen(DataLib.icons.player_white, "White to move")
             else:
                 self.ui.update_move_screen(DataLib.icons.player_black, "Black to move")
