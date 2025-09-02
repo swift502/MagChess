b524665d87f982388b97ada1385c5c61da72f6d0
Jan Bláha <blaha.j502@gmail.com>
2025-09-02 11:11:26 +0200

    install update

--- rpi_install.sh
+++ rpi_install.sh
@@ -1,6 +1,12 @@
 sudo apt update
 sudo apt install mpv libmpv2 libmpv-dev
 
+sudo grep -q '^dtoverlay=i2c1,pins_2_3,baudrate=' /boot/firmware/config.txt \
+  && sudo sed -i 's/^dtoverlay=i2c1,pins_2_3,baudrate=.*/dtoverlay=i2c1,pins_2_3,baudrate=400000/' /boot/firmware/config.txt \
+  || echo 'dtoverlay=i2c1,pins_2_3,baudrate=400000' | sudo tee -a /boot/firmware/config.txt
+
 arch=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 mkdir -p ~/.local/lib
 ln -s /usr/lib/$arch/libmpv.so.2 ~/.local/lib/libmpv.so.1
+
+zenity --info --text="Please reboot"
