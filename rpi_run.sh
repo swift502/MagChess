e7c0ea2781fa763504ff0d297db2fa7d72061cbb
Jan Bláha <blaha.j502@gmail.com>
2025-09-03 14:01:20 +0200

    bash

--- rpi_run.sh
+++ rpi_run.sh
@@ -2,15 +2,12 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
-# Update repository
 git restore .
 git pull
 
-# Activate virtualenv
 source ".venv/bin/activate"
 
 ARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 export LD_LIBRARY_PATH="$HOME/.local/lib:/usr/lib/$ARCH:$LD_LIBRARY_PATH"
 
-# Launch app
 python "app/__main__.py"
