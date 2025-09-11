50a91169e4f666e725406dc82f5b61191eec61a9
Jan Bláha <blaha.j502@gmail.com>
2025-09-12 00:21:18 +0200

    scoreboard css v2

--- scoreboard/src/scripts/utilities.ts
+++ scoreboard/src/scripts/utilities.ts
@@ -2,14 +2,13 @@ export function formatTimestamp(ts: string)
 {
     const date = new Date(ts);
     const dateFormat = new Intl.DateTimeFormat('cs-CZ', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
+        day: 'numeric',
+        month: 'numeric',
         hour: '2-digit',
         minute: '2-digit',
         hourCycle: 'h23', // ensure 00–23
     });
 
     const parts = Object.fromEntries(dateFormat.formatToParts(date).map(p => [p.type, p.value]));
-    return `${parts.day}.${parts.month}.${parts.year} ${parts.hour}:${parts.minute}`;
+    return `${parts.day}.${parts.month}. ${parts.hour}:${parts.minute}`;
 }
