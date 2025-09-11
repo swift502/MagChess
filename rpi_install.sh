49b92c8ec06c08aa76e3735e922e172494c51211
Jan Bláha <blaha.j502@gmail.com>
2025-09-11 18:21:19 +0200

    Update rpi_install.sh

--- rpi_install.sh
+++ rpi_install.sh
@@ -11,6 +11,8 @@ arch=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 mkdir -p ~/.local/lib
 ln -s /usr/lib/$arch/libmpv.so.2 ~/.local/lib/libmpv.so.1
 
+if [ ! -d ".venv" ]; then
+    python -m venv .venv
 source ".venv/bin/activate"
 pip install -r requirements.txt
 deactivate
