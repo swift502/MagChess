690d83b65af880604f7650ad07c864530eac1bd4
Jan Bláha <blaha.j502@gmail.com>
2025-09-11 11:46:47 +0200

    Update rpi_install.sh

--- rpi_install.sh
+++ rpi_install.sh
@@ -3,8 +3,6 @@
 sudo apt update
 sudo apt install mpv libmpv2 libmpv-dev
 
-
-
 sudo grep -q '^dtoverlay=i2c1,pins_2_3,baudrate=' /boot/firmware/config.txt \
   && sudo sed -i 's/^dtoverlay=i2c1,pins_2_3,baudrate=.*/dtoverlay=i2c1,pins_2_3,baudrate=400000/' /boot/firmware/config.txt \
   || echo 'dtoverlay=i2c1,pins_2_3,baudrate=400000' | sudo tee -a /boot/firmware/config.txt
@@ -15,3 +13,4 @@ ln -s /usr/lib/$arch/libmpv.so.2 ~/.local/lib/libmpv.so.1
 
 source ".venv/bin/activate"
 pip install -r requirements.txt
+deactivate
