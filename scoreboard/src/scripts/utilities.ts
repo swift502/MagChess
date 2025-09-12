b347578741cf76486053b20013741244ec04af1e
Jan Bláha <blaha.j502@gmail.com>
2025-09-12 13:57:43 +0200

    styles

--- scoreboard/src/scripts/utilities.ts
+++ scoreboard/src/scripts/utilities.ts
@@ -12,3 +12,20 @@ export function formatTimestamp(ts: string)
     const parts = Object.fromEntries(dateFormat.formatToParts(date).map(p => [p.type, p.value]));
     return `${parts.day}.${parts.month}. ${parts.hour}:${parts.minute}`;
 }
+
+export function formatTimestampTooltip(ts: string)
+{
+    const date = new Date(ts);
+    const dateFormat = new Intl.DateTimeFormat('cs-CZ', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hourCycle: 'h23', // ensure 00–23
+    });
+
+    const parts = Object.fromEntries(dateFormat.formatToParts(date).map(p => [p.type, p.value]));
+    return `${parts.day}.${parts.month}.${parts.year}. ${parts.hour}:${parts.minute}:${parts.second}`;
+}
